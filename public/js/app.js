0b44996e6a70ce3c9b8b8fff13ae2e5f91c83636
Michael Farmer <mfarmer91@gmail.com>
2017-08-30 14:02:45 -0400

    separated AddFormButton from EditableInventoryForm component and added SendButton component

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,19 @@
-class InventoryForm extends React.Component {
+class InventoryFormList extends React.Component {
+    render() {
+        return (
+            <div>
+                <EditableInventoryForm />,
+                <SendButton />
+            </div>
+        );
+    }
+}
+
+class EditableInventoryForm extends React.Component {
     render() {
         return (
             <div className='ui centered card'>
                 <div className='content'>
-          
                     <div className='ui form'>
                         <div className='field'>
                             <label>SKU <input type='text' name='sku' placeholder='Input SKU here.' /> </label>
@@ -11,12 +21,7 @@ class InventoryForm extends React.Component {
                         <div className='field'>
                             <label>Amount <input type='text' name='sku' placeholder='Input amount here.' /> </label>
                         </div>
-                        <div className="ui center aligned">
-                            <button className="ui basic green button icon">
-                                <i className="plus square outline icon"></i>
-                                Add SKU
-                            </button>
-                        </div>
+                        <AddFormButton />
                     </div>
                 </div>
             </div>
@@ -24,7 +29,39 @@ class InventoryForm extends React.Component {
     }
 }
 
+class AddFormButton extends React.Component {
+    render() {
+        return(
+            <div>
+                <div className="ui divider"></div>
+                <div className="ui center aligned">
+                    <button className="ui basic green button icon">
+                        <i className="plus square outline icon"></i>
+                        Add SKU
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+class SendButton extends React.Component {
+    render() {
+        return(
+            <div>
+            <div className="ui divider"></div>
+            <div className='ui basic content center aligned segment'>
+              <button className='ui basic blue button icon'>
+                <i className="checkmark icon"></i>
+                Send to BOH
+              </button>
+            </div>    
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-  <InventoryForm />,
+  <InventoryFormList />,
   document.getElementById('content')
 );
